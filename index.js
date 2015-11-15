e0c9d1cd34e764b4f5be7b85d4a757131d04f930
Wayne Burkett <wayne.burkett@gmail.com>
2015-11-15 16:54:04 -0600

    Add basic error handler

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
 
+app.use(function(err, req, res, next) {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
+
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 

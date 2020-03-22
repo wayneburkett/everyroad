1f85aebb6b8fd4e9fad4f536d2ca9cc2903d446a
Wayne Burkett <wayne.burkett@gmail.com>
2020-03-22 02:03:21 -0400

    Add Strava auth
    Based on the following examples:
    
    * https://developer.okta.com/blog/2018/08/21/build-secure-rest-api-with-node
    * https://www.sohamkamani.com/blog/javascript/2018-06-24-oauth-with-node-js/

--- app.js
+++ app.js
@@ -1,11 +1,30 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
+const axios = require('axios')
 
 const app = express()
 app.use(morgan('combined'))
+app.use(express.static(__dirname + '/public'))
+
+const { ISSUER, CLIENT_ID, CLIENT_SECRET, SCOPE } = process.env
+
+app.get('/oauth/redirect', (req, res) => {
+  const requestToken = req.query.code
+  axios({
+    method: 'post',
+    url: `${ISSUER}/token?client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&code=${requestToken}&grant_type=authorization_code`,
+    headers: {
+         accept: 'application/json'
+    }
+  }).then((response) => {
+    const accessToken = response.data.access_token
+    res.redirect(`/welcome.html?access_token=${accessToken}`)
+  })
+})
 
 app.get('/', (req, res) => {
-  console.log('Responding to route route')
+  console.log('Responding to root route')
   res.send('Hello, world!')
 })
 

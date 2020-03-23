4fcfb48ec707a13d0e0768c0d4ab42a9c46558a9
Wayne Burkett <wayne.burkett@gmail.com>
2020-03-23 00:06:40 -0400

    Redirect to / if there's an error

--- app.js
+++ app.js
@@ -11,15 +11,18 @@ const { ISSUER, CLIENT_ID, CLIENT_SECRET, SCOPE } = process.env
 
 app.get('/oauth/redirect', (req, res) => {
   const requestToken = req.query.code
+  let request = `${ISSUER}/token?client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&code=${requestToken}&grant_type=authorization_code`
   axios({
     method: 'post',
-    url: `${ISSUER}/token?client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&code=${requestToken}&grant_type=authorization_code`,
+    url: request,
     headers: {
          accept: 'application/json'
     }
   }).then((response) => {
     const accessToken = response.data.access_token
     res.redirect(`/welcome.html?access_token=${accessToken}`)
+  }).catch((error) => {
+    res.redirect('/')
   })
 })
 

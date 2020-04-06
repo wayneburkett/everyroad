576407f948c22765a36cc19834db17110e51b25f
Wayne Burkett <wayne.burkett@gmail.com>
2020-04-06 00:47:32 -0400

    Paginate

--- controllers/activities.js
+++ controllers/activities.js
@@ -6,7 +6,13 @@ const axios = require('axios')
 const BASE_API_URL = 'https://www.strava.com/api/v3/'
 
 module.exports.getActivities = (req, res) => {
-  get(req, res, 'athlete/activities')
+  get(req, res, 'athlete/activities', {
+    // 100 is the most you can get per page. we'll need to make queries
+    // until we get a response with fewer than 100 results (indicating that it's
+    // the last page) and then we'll batch them all up into one response
+    per_page: 100,
+    page: req.query.page
+  })
 }
 
 module.exports.getActivity = (req, res) => {

029a1e6f02e733fd67ef05fb55a3d5c30a07bff9
Wayne Burkett <wayne.burkett@gmail.com>
2020-04-12 20:13:34 -0400

    Refactor main map with react-map-gl

--- web/src/index.js
+++ web/src/index.js
@@ -1,74 +1,73 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
-import mapboxgl from 'mapbox-gl'
+import { render } from 'react-dom'
+import MapGL from 'react-map-gl'
+import ControlPanel from './control-panel'
 
-mapboxgl.accessToken = 'pk.eyJ1IjoibHdidXJrIiwiYSI6ImNqZ21mbm9pdDFiZXgzM21uaTVrNWpqNW4ifQ.d-nFW-zZRUKXM5E8rdgW3Q';
+const MAPBOX_TOKEN = 'pk.eyJ1IjoibHdidXJrIiwiYSI6ImNqZ21mbm9pdDFiZXgzM21uaTVrNWpqNW4ifQ.d-nFW-zZRUKXM5E8rdgW3Q'
 
 class Application extends React.Component {
-  mapRef = React.createRef();
-  map;
-
-  constructor(props: Props) {
-    super(props);
+  constructor (props) {
+    super(props)
     this.state = {
-      streams: []
-    };
+      viewport: {
+        latitude: 37.8,
+        longitude: -122.4,
+        zoom: 14,
+        bearing: 0,
+        pitch: 0
+      }
+    }
   }
 
-  componentDidUpdate() {
-    this.setFill();
+  _onLoad () {
+    this.fetchActivities()
   }
 
-  componentDidMount() {
-    this.map = new mapboxgl.Map({
-      container: this.mapRef.current,
-      style: 'mapbox://styles/mapbox/light-v10',
-      center: [-90.258403, 38.606557],
-      zoom: 5
-    });
-
-    this.map.on('load', () => {
-      this.fetchActivities()
-      this.setFill()
-    })
-  }
-
-  fetchActivities() {
-    fetch('/activities')
-      .then(result => result.json())
-      .then(data => {
-        this.setState({streams: data})
-      })
-  }
-
-  setFill() {
-    // update
-  }
-
-  render() {
-    const streams = this.state.streams
+  render () {
+    const streams = this.state.streams || []
     const renderStream = (stream, i) => {
       return (
         <div key={i} className='txt-s'>
           <span>{`${stream.name.toLocaleString()}`}</span>
         </div>
-      );
+      )
     }
-
     return (
-      <div>
-        <div ref={this.mapRef} className="absolute top right left bottom" />
-        <div className="bg-white absolute top lef ml12 mt12 py12 px12 shadow-darken10 round z1 wmax180">
-          <div className='mb6'>
-            <h2 className="txt-bold txt-s block">Rides</h2>
-            <p className='txt-s color-gray'>Your rides</p>
+      <MapGL
+        {...this.state.viewport}
+        width='100vw'
+        height='100vh'
+        mapStyle='mapbox://styles/mapbox/dark-v9'
+        onViewportChange={viewport => this.setState({ viewport })}
+        onLoad={_ => this._onLoad()}
+        mapboxApiAccessToken={MAPBOX_TOKEN}
+      >
+        <div>
+          <div className='bg-white absolute top lef ml12 mt12 py12 px12 shadow-darken10 round z1 wmax180'>
+            <div className='mb6'>
+              <h2 className='txt-bold txt-s block'>Rides</h2>
+              <p className='txt-s color-gray'>Your rides</p>
+            </div>
+            {streams.map(renderStream)}
           </div>
-          {streams.map(renderStream)}
         </div>
-      </div>
-    );
+      </MapGL>
+    )
+  }
+
+  componentDidUpdate () {
+  }
+
+  componentDidMount () {
+  }
+
+  fetchActivities () {
+    fetch('/activities')
+      .then(result => result.json())
+      .then(data => {
+        this.setState({ streams: data })
+      })
   }
 }
 
-ReactDOM.render(<Application />, document.getElementById('app'));
-
+render(<Application />, document.getElementById('app'))

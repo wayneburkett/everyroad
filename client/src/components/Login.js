5569166576fee5c6040a8079fede88280d933077
Wayne Burkett <wayne.burkett@gmail.com>
2020-04-28 00:11:45 -0400

    Simplify the login markup

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -3,17 +3,9 @@ import { StravaButton } from './StravaButton'
 
 export const Login = () => {
   return (
-    <div className='bg-darken10 viewport-half'>
-      <div className='flex-parent flex-parent--center-main pt36'>
-        <div className='flex-child bg-white round relative w600'>
-          <div className='px24 py24'>
-            <div className='txt-l mb12'>Modal title</div>
-            <div className='txt-m'>
+    <div className='login-overlay'>
               <StravaButton />
-            </div>
-          </div>
-        </div>
-      </div>
     </div>
   )
+
 }

83c08d7b278ae1a400dd9326041155d9049be125
Wayne Burkett <wayne.burkett@gmail.com>
2020-04-29 00:45:43 -0400

    Minor formatting that definitely matters

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -4,8 +4,7 @@ import { StravaButton } from './StravaButton'
 export const Login = () => {
   return (
     <div className='login-overlay'>
-              <StravaButton />
+      <StravaButton />
     </div>
   )
-
 }

c618ea4ea64ea0f2c297d6fdd62704eb787de492
Wayne Burkett <wayne.burkett@gmail.com>
2020-05-08 23:49:05 -0400

    Minor code formatting

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -1,10 +1,14 @@
 const utils = require('../utils/responses')
 
-// use as route middleware for any route that requires authentication
+/**
+ * Use as route middleware for any route that requires authentication
+ *
+ * @param {} req
+ * @param {} res
+ * @param {} next
+ */
 module.exports = (req, res, next) => {
   // this just checks for the existence of req.session.passport.user
-  if (req.isAuthenticated()) {
-    return next()
-  }
+  if (req.isAuthenticated()) return next()
   return utils.createResponder(res).unauthorized()
 }

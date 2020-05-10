3c3cd58b153196735c6fa7e38aa644e86fa397ea
Wayne Burkett <wayne.burkett@gmail.com>
2020-05-09 23:12:12 -0400

    Add docs

--- utils/responses.js
+++ utils/responses.js
@@ -1,5 +1,17 @@
-
+/**
+ * Creates a responder object with methods that operate on the given
+ * Response object. These methods send a consistently-formatted JSON
+ * object. API methods should always use one of the methods here and
+ * never create their own ad hoc responses.
+ *
+ * @param {} res an Express Response object
+ */
 module.exports.createResponder = function (res) {
+  /**
+   * Sends an HTTP 200 Success response with the given data.
+   *
+   * @param {} data
+   */
   const success = (data) => {
     return res.status(200).json({
       success: true,
@@ -7,6 +19,14 @@ module.exports.createResponder = function (res) {
     })
   }
 
+  /**
+   * Sends an HTTP error response with the given status and message.
+   *
+   * Defaults to HTTP 500 Server Error.
+   *
+   * @param {} statusCode
+   * @param {} message
+   */
   const failure = (statusCode = 500, message = 'Server Error') => {
     return res.status(statusCode).json({
       success: false,
@@ -17,10 +37,18 @@ module.exports.createResponder = function (res) {
     })
   }
 
+  /**
+   * Sends an HTTP 401 Unauthorized response.
+   */
   const unauthorized = () => {
     return failure(401, 'Unauthorized')
   }
 
+  /**
+   * Sends an HTTP error response built from the given HTTP error object.
+   *
+   * @param {} an HTTP error object
+   */
   const error = ({ response = {} } = {}) => {
     return failure(response.statusCode, response.statusText)
   }
